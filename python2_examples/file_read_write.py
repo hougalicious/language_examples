58ccfb64e886e91caba67ae999e83b6bbbd2be17
Matt Houglum <mhoug89@gmail.com>
2014-01-28 11:51:45 -0600

    renamed files follow naming scheme, added content to file_read_write to conform to python2

---
+++ python2_examples/file_read_write.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+import sys
+
+# open a file
+inFile = open('file_read_write.py', 'r')
+# 'a' for append
+# 'w' for write
+# 'r' for read
+# 'r+' for read+write
+# You can append 'b' to the end of any of these to r/w file as a byte file.
+# This should be done for files that contain non-text values
+
+
+# read an entire file all at once:
+fileString = inFile.read()  #optional: read() takes sizeInBytes as arg
+print(fileString)
+del fileString  # delete this reference --> garbage collect that memory
+
+### Using seek():
+# Go to the 1st byte of the file (byteNum is zero-indexed)
+inFile.seek(0)  # same as inFile.seek(0, 0)
+# 2nd arg: '2' means file end, '1' means cur position, '0' means beginning
+#inFile.seek(-3, 2)     # 3rd byte before the end
+print(inFile.tell()) # gives current position in file, should be 0 now
+
+
+# read one line in a file:
+fileString = inFile.readline()
+print(fileString)
+inFile.seek(0)  # reset file for next example
+
+# read in the lines in a file into a list:
+for line in inFile.readlines():
+    sys.stdout.write(line)     # end='' avoids 2x linebreaks
+inFile.seek(0)
+# OR:
+for line in list(inFile):
+    sys.stdout.write(line)
+inFile.seek(0)
+
+
+# loop over the lines in the file object, being memory efficient - this
+# doesn't read all the lines in at once!
+for line in inFile:
+    sys.stdout.write(line)     # end='' avoids 2x linebreaks
+inFile.seek(0)
+
+
+# write to a file:
+outFile = open('outFile.example', 'w')  # overwrites file if it exists
+outFile.write('This is a test of writing to a file.')
+outFile.write('\nDon\'t forget to manually add newlines yourself!\n')
+
+
+# close a file when you're done with it
+outFile.close()
+inFile.close()
